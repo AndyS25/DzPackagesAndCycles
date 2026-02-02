0c5b2120f7b24322d0702a1da8b0cd747e614248
Andrey Senkovich <senkovich@gmail.com>
2026-02-02 11:54:57 +0300

    simple calculation

--- src/main/java/ru/netology/javaqa/services/CalculatorVacationService.java
+++ src/main/java/ru/netology/javaqa/services/CalculatorVacationService.java
@@ -1,4 +1,3 @@
-//package ru.netology.javaqa;
 package ru.netology.javaqa.services;
 
 public class CalculatorVacationService {
@@ -10,13 +9,8 @@ public class CalculatorVacationService {
             int balans = money - expenses; //остаток накоплений после обязательных месячных трат
             if (money >= threshold) { // решаем как проводим месяц: работать или отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                int expensesVacation = balans - (balans / 3); //считаем траты на отдых
-                System.out.println("Месяц " + month + ". Денег " + money + ". Буду отдыхать. Потратил -" + expenses +
-                        ", затем ещё -" + expensesVacation);
-                money = balans - expensesVacation;
+                money = balans / 3;
             } else {
-                System.out.println("Месяц " + month + ". Денег " + money + ". Придётся работать. Заработал +" +
-                        income + ", потратил -" + expenses);
                 money = balans + income;
             }
         }
